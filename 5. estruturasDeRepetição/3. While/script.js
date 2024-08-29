e1f43b153cefaabc6c94634962c3b19046cae34e
Nicole Ferreira Mello <166025625+nicolefemello@users.noreply.github.com>
2024-08-29 20:46:08 -0300

    exercícios

--- 5. estruturasDeRepetição/3. While/script.js
+++
@@ -1,8 +0,0 @@
-function criarVariavel(qntdVar, tipoDado) {
-    let parametros = [];
-    for (let i = 0; i < qntdVar; i++) {
-        if (tipoDado.toLowerCase() == 'number') {
-            parametros = {}
-        } else (tipoDado.toLowerCase() == 'string')
-    }
-}
