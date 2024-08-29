ace55a1ca1d732f2faf05da9c92ea7d8d7f3d6ce
nicolefemello <nicfmello@gmail.com>
2024-08-29 13:32:32 -0300

    exercícios

---
+++ 5. estruturasDeRepetição/3. While/script.js
@@ -0,0 +1,8 @@
+function criarVariavel(qntdVar, tipoDado) {
+    let parametros = [];
+    for (let i = 0; i < qntdVar; i++) {
+        if (tipoDado.toLowerCase() == 'number') {
+            parametros = {}
+        } else (tipoDado.toLowerCase() == 'string')
+    }
+}
